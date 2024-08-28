12905c5d4c9542c600ea0e7b7b02ee78930d3da4
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-08-28 12:12:20 +0300

    useReducer hook use case scenario

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+if(action.type ==='increment'){
+  return {count: state.count +1}}
+else if (action.type === 'decrement'){
+  if(state.count === 0){
+    alert('Count cannot be less than zero')
+    return state
+  }
+  return {count: state.count -1}
+}
+
+  }
+
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+  const handleIncrement = () => {
+    dispatch({type: 'increment'})
+  
+  }
+
+  const handleDecrement = () => {
+    dispatch({type: 'decrement'})
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {handleIncrement}>Increment</button>
+      <p>Count: {state.count}</p>
+      <button onClick = {handleDecrement}>Decrement</button>
+      
     </div>
   );
 }
