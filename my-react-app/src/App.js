be631b7d1be691015bf28aafe7f945e3ce50533a
PetyaMihaylova <mihaylovapetya27@gmail.com>
2024-08-29 11:17:41 +0300

    switch case and if-function in reducer

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,38 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import { useReducer } from 'react';
-
-const reducer = (state, action) => {
-if(action.type ==='increment'){
-  return {count: state.count +1}}
-else if (action.type === 'decrement'){
-  if(state.count === 0){
-    alert('Count cannot be less than zero')
-    return state
-  }
-  return {count: state.count -1}
+const ACTIONS = {
+  increment: 'increment',
+  decrement: 'decrement'
 }
 
+const reducer = (state, action) => {
+  switch(action.type){
+    case ACTIONS.increment:
+      return {...state, count: state.count +1};
+    case ACTIONS.decrement:
+      return {...state, count: state.count -1};
+    default: throw new Error('Wrong Action')
   }
 
+//instead of switch case (lines 10-16), we can have the body of the reducer described with if-function
+// if(action.type === ACTIONS.increment){
+//   return {...state, count: state.count +1}}
+// else if (action.type === ACTIONS.decrement){
+//   if(state.count === 0){
+//     alert('Count cannot be less than zero')
+//     return state.count
+//   }
+//   return {...state, count: state.count -1}
+// }
+// else {
+//   throw new Error('Wrong Action')
+// }
+  }
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {count: 0})
+  const [state, dispatch] = useReducer(reducer, {count: 0, user: 'g', age: 24})
   const handleIncrement = () => {
-    dispatch({type: 'increment'})
+    dispatch({type: ACTIONS.increment})
   
   }
 
   const handleDecrement = () => {
-    dispatch({type: 'decrement'})
+    dispatch({type: ACTIONS.decrement})
   
   }
+  console.log(state);
+  
   return (
     <div className="App">
       <button onClick = {handleIncrement}>Increment</button>
-      <p>Count: {state.count}</p>
+      <p>Count: {state?.count}</p>
+      <p>User: {state?.user}</p>
+      <p>Age: {state?.age}</p>
       <button onClick = {handleDecrement}>Decrement</button>
+      <button onClick = {()=> dispatch({type: 'asdasda'})}>Wrong Action</button>
       
     </div>
   );
